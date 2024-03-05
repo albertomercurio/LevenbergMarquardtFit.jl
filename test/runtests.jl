41af28a7370b2b365561bb2c7d1917633acbba16
Alberto Mercurio <alberto.mercurio96@gmail.com>
2024-03-05 18:48:13 +0100

    Writing base code

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,13 +2,35 @@ using LevenbergMarquardtFit
 using Test
 using Aqua
 using JET
+using LinearAlgebra
 
 @testset "LevenbergMarquardtFit.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(LevenbergMarquardtFit; ambiguities = false,)
+    function f(x,p)
+        @. p[1] * exp(-(x - p[3])^2 / $(2 * p[2]^2))
     end
-    @testset "Code linting (JET.jl)" begin
-        JET.test_package(LevenbergMarquardtFit; target_defined_modules = true)
-    end
-    # Write your tests here.
+    
+    xdata = range(0, 10, 10000) |> collect
+    
+    p0 = [1, 9, 0.1]
+    p_correct = [2, 1, 5]
+    ydata = f(xdata, (p_correct)) .+ 0.0001*randn(length(xdata))
+    
+    lower = [0, 0, 0.0]
+    upper = [10.0, 20, 10.0]
+
+    p_opt = curve_fit(f, xdata, ydata, p0, lower=lower, upper=upper).params
+
+    ADM = AutoDiffMethod(f, p0, xdata);
+    p_opt_auto = curve_fit(f, xdata, ydata, p0, lower=lower, upper=upper, diff_method=ADM).params
+
+    @test p_opt ≈ p_correct atol=1e-5
+    @test p_opt_auto ≈ p_correct atol=1e-5
+end
+
+@testset "Code quality (Aqua.jl)" begin
+    Aqua.test_all(LevenbergMarquardtFit; ambiguities = false,)
+end
+
+@testset "Code linting (JET.jl)" begin
+    JET.test_package(LevenbergMarquardtFit; target_defined_modules = true)
 end
