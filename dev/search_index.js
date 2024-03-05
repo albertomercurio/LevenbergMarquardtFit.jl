aefe817933020ad2c1446ec2628fa032b5654cd5
Documenter.jl <documenter@juliadocs.github.io>
2024-03-05 18:07:24 +0000

    build based on 38646a8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LevenbergMarquardtFit","category":"page"},{"location":"#LevenbergMarquardtFit","page":"Home","title":"LevenbergMarquardtFit","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LevenbergMarquardtFit.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LevenbergMarquardtFit]","category":"page"}]
+}
